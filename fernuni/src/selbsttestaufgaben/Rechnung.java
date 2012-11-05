db582f7039b456f677a2bf7f26d6b8e569a993f0
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-11-05 22:05:47 +0100

    Erweiterung um Arrays

--- fernuni/src/selbsttestaufgaben/Rechnung.java
+++ fernuni/src/selbsttestaufgaben/Rechnung.java
@@ -5,57 +5,89 @@ class Rechnung {
 	double betrag;
 	double rabatt;
 	Kunde rechnungsempfaenger;
-	
+	Rechnungsposten[] rechnungspositionen = new Rechnungsposten[100];;
+
 	public static void main(String[] args) {
 		String annasName = new String("Anna Müller");
 		String annasAnschrift = "Mühlenweg 2, 12345 Irgendwo";
 		Kunde kunde1 = new Kunde(annasName, annasAnschrift);
 		Rechnung rechnung6 = new Rechnung();
 		rechnung6.rechnungsempfaenger = kunde1;
-		rechnung6.fuegePostenHinzu(25, 1.76);
+
+		rechnung6.fuegePostenHinzu(25, new Artikel(13235354, "Kartoffel", 1.25,
+				0.19));
+		rechnung6
+				.fuegePostenHinzu(25, new Artikel(2345678, "Eier", 1.50, 0.19));
+		rechnung6.fuegePostenHinzu(25, new Artikel(9876573, "Zitronen", 0.99,
+				0.19));
 		rechnung6.legeRabattFest(0.04);
 		rechnung6.gebeAus();
 	}
-	
-	double legeMehrwertsteuerFest(double nettopreis, double mwstSatz) {
+
+	double berechneBruttopreis(Rechnungsposten[] rechnungspositionen) {
 		double bruttopreis = 0;
-		bruttopreis = nettopreis * mwstSatz;		
+		for (int i = 0; i < rechnungspositionen.length; i++) {
+			if (rechnungspositionen[i] != null) {
+				bruttopreis = bruttopreis
+						+ rechnungspositionen[i].artikel.preis
+						+ rechnungspositionen[i].artikel.preis
+						* rechnungspositionen[i].artikel.mehrwertsteuer;
+			} else
+				break;
+		}
 		return bruttopreis;
+
 	}
-	
-	void fuegePostenHinzu(int anzahl, double einzelpreis) {
-		this.betrag += anzahl * einzelpreis;
+
+	public void fuegePostenHinzu(int anzahl, Artikel artikel) {
+		for (int i = 0; i < this.rechnungspositionen.length; i++) {
+			if (this.rechnungspositionen[i] == null) {
+				this.rechnungspositionen[i] = new Rechnungsposten(anzahl,
+						artikel);
+				break;
+			}
+
+		}
+		// this.betrag += anzahl * einzelpreis;
 	}
-	
+
 	void legeRabattFest(final double neuerRabatt) {
 		this.rabatt = neuerRabatt;
 	}
-	
+
 	void legeRechnungsempfaengerFest(Kunde empfaenger) {
 		this.rechnungsempfaenger = empfaenger;
 	}
-	
+
 	double liefereRabatt() {
 		return this.rabatt;
 	}
-	
+
 	Kunde liefereRechnungsempfaenger() {
 		return this.rechnungsempfaenger;
 	}
-	
+
 	double berechneNettopreis() {
 		return this.betrag * (1 - this.liefereRabatt());
 	}
-	
+
 	void gebeAus() {
 		System.out.println("An:");
-		System.out.println(this.liefereRechnungsempfaenger().
-				liefereName());
-		System.out.println(this.liefereRechnungsempfaenger().
-				liefereAnschrift());
+		System.out.println(this.liefereRechnungsempfaenger().liefereName());
+		System.out
+				.println(this.liefereRechnungsempfaenger().liefereAnschrift());
+		for (int i = 0; i < rechnungspositionen.length; i++) {
+			if (rechnungspositionen[i] != null) {
+
+				System.out.println(i + 1 + ": " + this.rechnungspositionen[i]);
+
+			} else
+				break;
+		}
+
 		System.out.print("Netto: ");
 		System.out.println(this.berechneNettopreis());
+		System.out.println("Brutto: ");
+		System.out.println(this.berechneBruttopreis(rechnungspositionen));
 	}
-	
-
 }
--- fernuni/src/selbsttestaufgaben/Rechnungsposten.java
+++ fernuni/src/selbsttestaufgaben/Rechnungsposten.java
@@ -3,29 +3,41 @@ package selbsttestaufgaben;
 class Rechnungsposten {
 	int anzahl;
 	Artikel artikel;
-	
+
+	// TO DO: Gesamt-Nettopreis für die Position berechnen
+
 	Rechnungsposten(final int anzahl, final Artikel artikel) {
 		this.legeAnzahlFest(anzahl);
 		this.legeArtikelFest(artikel);
 	}
-	
+
+	@Override
+	public String toString() {
+		StringBuilder stb = new StringBuilder();
+		stb.append(anzahl);
+		stb.append(" x ");
+		stb.append(artikel.beschreibung);
+		stb.append(" Euro ");
+		stb.append(artikel.preis);
+		return stb.toString();
+	}
+
 	void legeAnzahlFest(final int neueAnzahl) {
 		this.anzahl = neueAnzahl;
 	}
-	
+
 	void legeArtikelFest(final Artikel neuerArtikel) {
 		this.artikel = neuerArtikel;
 	}
-	
+
 	double berechneGesamtbetrag() {
-		return this.liefereAnzahl()
-		* this.liefereArtikel().lieferePreis();
+		return this.liefereAnzahl() * this.liefereArtikel().lieferePreis();
 	}
-	
+
 	int liefereAnzahl() {
 		return this.anzahl;
 	}
-	
+
 	Artikel liefereArtikel() {
 		return this.artikel;
 	}
